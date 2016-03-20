153e842924931da5cc0b309cab6ba71a3badc4b5
mt-xing <mt_xing@live.com>
2016-03-20 18:39:32 -0400

    UPDATE: Webkit Home Page

--- test/panel.js
+++ test/panel.js
@@ -26,13 +26,14 @@ function panelHov(e){
 		document.getElementById("Panel2").className = "Panel";
 		document.getElementById("Panel3").className = "Panel NoBefore";
 		document.getElementById("Panel4").className = "Panel";
-		document.getElementById("Panel5").className = "Panel";*/	
-			document.getElementById("Head1").removeAttribute("style");	
-			document.getElementById("Head2").removeAttribute("style");	
-			document.getElementById("Head3").removeAttribute("style");	
-			document.getElementById("Head4").removeAttribute("style");	
-			document.getElementById("Head5").removeAttribute("style");
-		document.getElementById("Head3").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(17vw)";
+		document.getElementById("Panel5").className = "Panel";*/
+		
+		document.getElementById("Head3").style.opacity = 1;
+			document.getElementById("Head1").style.transform = "rotate(90deg)translateY(-1vw)";
+			document.getElementById("Head2").style.transform = "rotate(90deg)translateY(-11vw)";
+			document.getElementById("Head3").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(17vw)";
+			document.getElementById("Head4").style.transform = "rotate(90deg)translateY(-78vw)";
+			document.getElementById("Head5").style.transform = "rotate(90deg)translateY(-89vw)";
 		return;
 	}
 	
@@ -60,8 +61,16 @@ function panelHov(e){
 			document.getElementById("Head" + i).removeAttribute("style");
 		}
 	}
-	
+	if(e != 3){
+		document.getElementById("Head3").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(17vw)";
+		document.getElementById("Head3").style.opacity = 0;
+	} else{
+		document.getElementById("Head3").style.opacity = 1;
+	}
 	switch(e){
+		case 1:
+			document.getElementById("Head2").style.transform = "rotate(90deg)translateY(-60vw)";
+			break;
 		case 2:
 			document.getElementById("Head2").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(10vw)";
 			document.getElementById("Panel1").style.background = "rgba(192, 57, 43, 0)";
@@ -75,6 +84,7 @@ function panelHov(e){
 			document.getElementById("Panel3").style.background = "rgba(44, 62, 80, 0)";
 			break;
 		case 5:
+			document.getElementById("Head4").style.transform = "rotate(90deg)translateY(-30vw)";
 			document.getElementById("Head5").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(35vw)";
 			document.getElementById("Panel4").style.background = "rgba(142, 68, 173, 0)";
 			break;
