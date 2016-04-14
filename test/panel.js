9ef634ba5c133dcf4e5f3b0ad726e9c745c5bd70
mt-xing <mt_xing@live.com>
2016-04-13 20:17:20 -0400

    UPDATE: Regionals 2016 Winners

--- test/panel.js
+++ test/panel.js
@@ -17,6 +17,7 @@ function panelHov(e){
 		document.getElementById("Panel3").style.opacity = 0.2;
 		document.getElementById("Panel4").style.opacity = 1;
 		document.getElementById("Panel5").style.opacity = 1;
+		document.getElementById("SplashBG").style.opacity = 1;
 		/*document.getElementById("Panel1").className = "Panel";	
 		document.getElementById("Panel2").className = "Panel";
 		document.getElementById("Panel3").className = "Panel NoBefore";
@@ -24,6 +25,11 @@ function panelHov(e){
 		document.getElementById("Panel5").className = "Panel";*/
 		
 		document.getElementById("Head3").style.opacity = 1;
+		document.getElementById("Head1").style.width = null;
+		document.getElementById("Head2").style.width = null;
+		document.getElementById("Head3").style.width = null;
+		document.getElementById("Head4").style.width = null;
+		document.getElementById("Head5").style.width = null;
 			document.getElementById("Head1").style.transform = "rotate(90deg)translateY(-1vw)";
 			document.getElementById("Head2").style.transform = "rotate(90deg)translateY(-11vw)";
 			document.getElementById("Head3").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(13vw)";
@@ -48,6 +54,7 @@ function panelHov(e){
 			//document.getElementById("Panel" + i).className = "Panel NoBefore";
 			
 			document.getElementById("Head" + i).style.transform = "rotate(0deg)translateY(calc(50vh - 50%))";
+			document.getElementById("Head" + i).style.width = "60vw";
 		} else if(i > e){
 			el.style.transform = "translateX(50vw)scale(1)";
 			document.getElementById("PanelBG" + i).style.opacity = 0;
@@ -57,18 +64,21 @@ function panelHov(e){
 		}
 	}
 	if(e != 3){
-		document.getElementById("Head3").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(17vw)";
+		document.getElementById("Head3").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(13vw)";
 		document.getElementById("Head3").style.opacity = 0;
+		document.getElementById("SplashBG").style.opacity = 0;
 	} else{
 		document.getElementById("Head3").style.opacity = 1;
+		document.getElementById("SplashBG").style.opacity = 1;	
 	}
 	
 	switch(e){
 		case 1:
 			document.getElementById("Head2").style.transform = "rotate(90deg)translateY(-60vw)";
+			document.getElementById("Head1").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(-5vw)";
 			break;
 		case 2:
-			document.getElementById("Head2").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(10vw)";
+			document.getElementById("Head2").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(5vw)";
 			document.getElementById("Panel1").style.background = "rgba(192, 57, 43, 0)";
 			break;
 		case 3:
@@ -76,7 +86,7 @@ function panelHov(e){
 			document.getElementById("Panel2").style.background = "rgba(31, 58, 147, 0)";
 			break;
 		case 4:
-			document.getElementById("Head4").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(28vw)";
+			document.getElementById("Head4").style.transform = "rotate(0deg)translateY(calc(50vh - 50%))translateX(22vw)";
 			document.getElementById("Panel3").style.background = "rgba(44, 62, 80, 0)";
 			break;
 		case 5:
