a7966b38cb61574635973448cbb85ed68cf989d5
mt-xing <mt_xing@live.com>
2017-05-01 13:18:16 -0400

    FIX: Home Page Resizing

--- pageJS/index.js
+++ pageJS/index.js
@@ -4,6 +4,16 @@ function newLoad(){
 	console.log("Home Page Loaded");
 	
 	s = skrollr.init({smoothScrolling: false});
+	
+	
+	
+	window.onresize = resize;
+	function resize()
+	{
+		//alert("resize event detected!");
+		s.destroy();
+		s = skrollr.init({smoothScrolling: false});
+	}
 }
 
 function newUnload(){
