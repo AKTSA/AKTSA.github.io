1b6cca79905cae8b0c500e635ae123f377beb7a0
agnim25 <agnim.agarwal@outlook.com>
2018-04-11 17:58:20 -0400

    EDIT: Font and navbar

--- pageJS/member.js
+++ pageJS/member.js
@@ -4,7 +4,7 @@ function newLoad(){
 }
 
 function newUnload(){
-	
+
 }
 
 
@@ -15,7 +15,7 @@ function loadData() {
 		if (this.readyState == 4 && this.status == 200) {
 			var el = JSON.parse(this.responseText);
 			console.log("Received following data from server: " + JSON.stringify(el));
-			
+
 			document.getElementById("MeetBox").textContent = "";
 			for(i = 0; i < el["meet"].length; i++){
 				var temp = document.createElement("LI");
@@ -23,9 +23,9 @@ function loadData() {
 				document.getElementById("MeetBox").appendChild(temp);
 				//document.getElementById("MeetBox").innerHTML += "<li>" + el["meet"][i] + "</li>";
 			}
-			
+
 			document.getElementById("CommBox").textContent = "";
-			for(i = 0; i < el['comm'].length; i++){
+			for(i = 12;i < el['comm'].length; i++){
 				var temp = document.createElement("LI");
 				temp.textContent = el["comm"][i]["date"];
 				var temp2 = document.createElement("P");
@@ -33,9 +33,9 @@ function loadData() {
 				temp.appendChild(temp2);
 				document.getElementById("CommBox").appendChild(temp);
 			}
-			
+
 			document.getElementById("MeetBox").getElementsByTagName("LI")[parseInt(el["nextMeet"])].className = "NextMeet";
-			
+
 		}
 	};
 	xhttp.open("GET", "https://aktsa.azurewebsites.net/data.php", true);
