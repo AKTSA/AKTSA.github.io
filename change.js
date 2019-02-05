0f066b680b2509d8ca3bcc933b3a5b6da8f0a31b
applsais <11185098+applsais@users.noreply.github.com>
2019-02-04 21:37:23 -0500

    Update change.js

--- change.js
+++ change.js
@@ -181,3 +181,12 @@ function setUpPage(){
 		}
 	});
 }
+
+  var xhttp = new XMLHttpRequest(); 
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
+  xhttp.open("GET", "https://aktsawebmasters.azurewebsites.net/ping.php?request=ping", true);
+  xhttp.send();
